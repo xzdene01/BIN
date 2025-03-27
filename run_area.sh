a1b2f93808f04e63b9eefcc48d83750a586917da
Jan Zdenek <zdenek.honza@email.cz>
2025-03-27 23:17:00 +0100

    changed nume epoch 5000 -> 2000

--- run_area.sh
+++ run_area.sh
@@ -11,6 +11,9 @@ tau_values=(0.10 0.18 0.32 0.58 1.05 1.90 3.42 6.17 11.11 20.00)
 # Loop over each tau and run the command n_runs times
 for tau in "${tau_values[@]}"; do
     for (( run=1; run<=n_runs; run++ )); do
-        python main.py -f generated/u_arrmul4.cgp -c area -p 10 -e 5000 -m 0.03 -t "$tau" -d cpu --log "logs/area_${tau}"
+        echo "================================="
+        echo "Running $run/$n_runs for tau=$tau"
+        echo "================================="
+        python main.py -f generated/u_arrmul4.cgp -c area -p 10 -e 2000 -m 0.03 -t "$tau" -d cpu --log "logs/area_${tau}"
     done
 done
--- run_error.sh
+++ run_error.sh
@@ -11,6 +11,9 @@ tau_values=(30 34 38 42 46 50 54 58 62 66)
 # Loop over each tau and run the command n_runs times
 for tau in "${tau_values[@]}"; do
     for (( run=1; run<=n_runs; run++ )); do
-        python main.py -f generated/u_arrmul4.cgp -c error -p 10 -e 5000 -m 0.03 -t "$tau" -d cpu --log "logs/error_${tau}" --pretrain 5000
+        echo "================================="
+        echo "Running $run/$n_runs for tau=$tau"
+        echo "================================="
+        python main.py -f generated/u_arrmul4.cgp -c error -p 10 -e 2000 -m 0.03 -t "$tau" -d cpu --log "logs/error_${tau}" --pretrain 5000
     done
 done
